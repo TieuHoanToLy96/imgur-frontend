35ba5dfa182c31c056f3e08f520645e20d6ff9a7
TieuHoanToLy96 <tieuhoantoly@gmail.com>
2020-02-25 22:20:37 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 const withSass = require('@zeit/next-sass')
 const path = require('path')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({})
+
 module.exports = withSass({
   webpack (config, options) {
     config.resolve.alias[''] = path.join(__dirname, '')
